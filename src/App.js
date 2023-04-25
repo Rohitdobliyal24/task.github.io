33c33e3ec92ebee2c683a83e64b7af4776d059e2
Samyak <samyaksk10@gmail.com>
2023-04-25 12:41:36 +0530

    Signup page creation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Signup from './components/signup'
 
 function App() {
   return (
-   <h1>hello</h1>
+  <>
+  <Signup/>
+  </>
   );
 }
 
