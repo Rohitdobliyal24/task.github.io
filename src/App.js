467f0eab4e9c96ee986d01321f87e0ff4888feb1
Samyak <samyaksk10@gmail.com>
2023-05-01 23:59:19 +0530

    Inventory section initialised

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import Signup from './/components/signup/signup'
 import Login from './/components/login/login'
-
+import Inventory from './/components/inventory/inventory'
 function App() {
   return (
    <>
   <Login/>
   <br /><br /> <br /><br /><br />
   <Signup/>
+  <br /><br /> <br /><br /><br />
+  <Inventory/>
    </>
   
   );
