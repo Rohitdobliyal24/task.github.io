03740e6193b79b919b0adbb09f3883e684b4b67e
Samyak <samyaksk10@gmail.com>
2023-06-01 19:45:30 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
-import Signup from './/components/signup/signup'
 import Login from './/components/login/login'
-import Inventory from './/components/inventory/inventory'
 function App() {
   return (
    <>
   <Login/>
-  <br /><br /> <br /><br /><br />
-  <Signup/>
-  <br /><br /> <br /><br /><br />
-  <Inventory/>
    </>
   
   );
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 import '../login/login.css'
+// import image from "../../assets/black.jpg"
 const signup = () => {
   return (
     <div className="login_page">
       <div className="img">
-        hey
-        {/* <img className="image" src={signupimage} alt="signup_img" /> */}
+        
+          <h1 id="head">100% uptime 😎</h1>
+          <h2 id="head1">Zero Infrastructure Management</h2>
       </div>
       <div className="content">
-        <h1>EazyGhar Broker</h1>
+        <h1>Welcome <span id="back">Back!</span> </h1>
 
         <div className="login_box">
-          <h2>Login</h2>
+          <h2>Glad to see you, Again!</h2>
            
-          <input type="text" name="number" id="number" className="input_fields" placeholder="Phone Number" required />
-          <input type="password" name="password" id="pass" className="input_fields" placeholder="Password" required />
-        
-          <input type="button" className="input_fields" id="submit" value="Create Account" />
+          <input type="email" name="number" id="number" className="input_fields" placeholder="Enter your email" required />
+          <input type="password" name="password" id="pass" className="input_fields" placeholder="Enter your Password" required />
+          <input type="button" className="input_fields" id="submit" value="Login" />
           <br />
-          <a href="#" id="to_signup">Create Account</a>
+          <a href="#" id="to_signup">Don't have an account yet? <a href="#">Signup</a> </a>
         </div>
         </div>
     </div>
